23fb98b6dc426c1c978b87a206308022009270bc
fld-test <1550954374@qq.com>
2020-06-17 10:59:42 +0800

    test

--- src/GitTest.java
+++ src/GitTest.java
@@ -1,5 +1,6 @@
 public class GitTest {
     public static void main(String[] args) {
         System.out.println("Test Git");
+        System.out.println("git分支");
     }
 }
--- src/GitTest01.java
+++ src/GitTest01.java
@@ -1,5 +1,6 @@
 public class GitTest01 {
     public static void main(String[] args) {
         System.out.println("=============");
+        System.out.println("git分支");
     }
 }
